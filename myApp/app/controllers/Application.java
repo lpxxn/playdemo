05a80afeec098987f86bd0f074b3badaa374530a
lpxxn <mi_duo@live.com>
2018-10-23 13:24:44 +0800

    test

--- myApp/app/controllers/Application.java
+++ myApp/app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -13,4 +14,12 @@ public class Application extends Controller {
         render();
     }
 
+
+    public static void sayHello(@Required String myName) {
+        if (validation.hasErrors()) {
+            flash.error("Oops, please enter your name!");
+            index();
+        }
+        render(myName);
+    }
 }
